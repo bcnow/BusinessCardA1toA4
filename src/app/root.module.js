399c8665ab2083d4ef56b8c15493a9bc49cf767a
bcnow <bcnow>
2017-10-22 22:52:25 +0300

    added simple ui-router from hello world example

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,4 +1,46 @@
-angular.module('root', [])
-	.config(function () {
+angular.module('root', ['ui.router'])
+	.config(function ($stateProvider) {
 		console.log('module root config');
+		var helloState = {
+			name: 'hello',
+			url: '/hello',
+			template: '<h3>hello world!</h3>'
+		}
+
+		var aboutState = {
+			name: 'about',
+			url: '/about',
+			template: '<h3>Its the UI-Router hello world app!</h3>'
+		}
+
+		$stateProvider.state(helloState);
+		$stateProvider.state(aboutState);
+
+		// let trace = window["angular-ui-router"].trace; // or "ui-router-ng2"
+		// trace.enable(); // Trace everything (very verbose)
+	}).run(function ($rootScope) {
+
+		console.log('module root run');
+
+		$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+			console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
+		});
+
+		$rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams) {
+			console.log('$stateChangeError - fired when an error occurs during transition.');
+			console.log(arguments);
+		});
+
+		$rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+			console.log('$stateChangeSuccess to ' + toState.name + '- fired once the state transition is complete.');
+		});
+
+		$rootScope.$on('$viewContentLoaded', function (event) {
+			console.log('$viewContentLoaded - fired after dom rendered', event);
+		});
+
+		$rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+			console.log('$stateNotFound ' + unfoundState.to + '  - fired when a state cannot be found by its name.');
+			console.log(unfoundState, fromState, fromParams);
+		});
 	});
