531d89c83a43e0836ea3a7e83e6b311748c3ac80
bcnow <bcnow>
2017-10-28 14:28:35 +0300

    copying bootstrap styles to dist

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,13 @@
 import gulp from 'gulp';
 import server from 'browser-sync';
+import del from 'del';
+var sass = require('gulp-sass');
+var gulpCopy = require('gulp-copy');
 
+const root = 'src/';
 const paths = {
 	dist: './dist/',
+	bower: './bower_components/',
 	distDocs: './docs/build',
 	docs: './docs/app/*.js',
 	scripts: [`${root}/app/**/*.js`, `!${root}/app/**/*.spec.js`],
@@ -28,17 +33,30 @@ const paths = {
 server.create();
 
 gulp.task('clean', () => {
-	return del([paths.dist + '**'], {
+	return del([paths.dist + '**/*', ], {
 		force: true
 	});
 });
+
 gulp.task('copy', ['clean'], () => {
 	return gulp.src(paths.static, {
 		base: 'src'
 	}).pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('styles', () => {});
+gulp.task('styles-3rd-party', ['clean'], () => {
+	return gulp.src(paths.bower + 'bootstrap/dist/css/bootstrap.min.css')
+		.pipe(gulp.dest(paths.dist + '/css'));
+});
+
+gulp.task('styles', ['styles-3rd-party'], () => {
+	return gulp.src(paths.styles)
+		.pipe(sass({
+			outputStyle: 'compact'
+		}).on('error', sass.logError))
+		.pipe(gulp.dest(paths.dist + '/css'));
+
+});
 gulp.task('serve', () => {
 	return server.init({
 		files: [`${paths.dist}/**`],
@@ -52,7 +70,5 @@ gulp.task('watch', function () {});
 
 gulp.task('default', [
 	'copy',
-	'styles',
-	'serve',
-	'watch'
+	'styles'
 ]);
