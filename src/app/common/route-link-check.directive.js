ad619ba4efd6f49068bc2fca3421def0aa9bd951
bcnow <bcnow>
2017-11-22 20:20:12 +0200

    added check route link check directive

---
+++ src/app/common/route-link-check.directive.js
@@ -0,0 +1,33 @@
+function routeLinkCheck() {
+	return {
+		restrict: 'E',
+		compile: function ($element) {
+			return function ($scope, $element, $attrs, $ctrl) {
+				if (_.isNil($element.attr("href")) && _.isNil($element.attr("ui-sref")) === false) {
+					$element.prop("disabled", true);
+					$element.attr('disabled', 'disabled');
+				}
+			};
+		}
+	};
+}
+
+/**
+ * @ngdoc directive
+ * @name lengthCheck
+ * @module components.contact
+ *
+ * @description
+ *
+ * Aenean ornare odio elit, eget facilisis ipsum molestie ac. Nam bibendum a nibh ut ullamcorper.
+ * Donec non felis gravida, rutrum ante mattis, sagittis urna. Sed quam quam, facilisis vel cursus at.
+ *
+ * @usage
+ *
+ * ### How to use
+ * Aenean ornare odio elit, eget facilisis ipsum molestie ac. Nam bibendum a nibh ut ullamcorper.
+ * Donec non felis gravida, rutrum ante mattis, sagittis urna. Sed quam quam, facilisis vel cursus at.
+ **/
+angular
+	.module('root')
+	.directive('a', routeLinkCheck);
